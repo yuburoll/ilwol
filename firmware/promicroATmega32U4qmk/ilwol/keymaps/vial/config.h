75330cc88bba1e228a4019320645c6f6ae199f4e
yuburoll <yuburoll@gmail.com>
2026-06-19 14:40:20 +0900

    add promicro firmware and vial

---
+++ firmware/promicroATmega32U4qmk/ilwol/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x79, 0x03, 0x89, 0x44, 0xBF, 0xD3, 0xEB, 0x51}
+#define VIAL_UNLOCK_COMBO_ROWS { 4, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
